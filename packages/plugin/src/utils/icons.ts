2be34982e66f24d9fcacee259c119381f1bced33
Miles Johnson <mileswjohnson@gmail.com>
2021-10-08 21:52:38 -0700

    deps: Update codicons.

--- packages/plugin/src/utils/icons.ts
+++ packages/plugin/src/utils/icons.ts
@@ -18,7 +18,7 @@ const KIND_ICONS: Record<ReflectionKind, string> = {
 	1024: 'symbol-property', // Property
 	2048: 'symbol-method', // Method
 	4096: 'symbol-method', // CallSignature
-	8192: 'json', // IndexSignature
+	8192: 'bracket-dot', // IndexSignature
 	16_384: 'symbol-method', // ConstructorSignature
 	32_768: 'symbol-property', // Parameter
 	65_536: 'symbol-key', // TypeLiteral
@@ -26,7 +26,7 @@ const KIND_ICONS: Record<ReflectionKind, string> = {
 	262_144: 'symbol-field', // Accessor
 	524_288: 'symbol-field', // GetSignature
 	1_048_576: 'symbol-field', // SetSignature
-	2_097_152: 'json', // ObjectLiteral
+	2_097_152: 'bracket', // ObjectLiteral
 	4_194_304: 'symbol-parameter', // TypeAlias
 	8_388_608: 'symbol-event', // Event
 	16_777_216: 'references', // Reference
