fb95fd22861222bef9379ffc749c034c31e7ab60
Miles Johnson <mileswjohnson@gmail.com>
2021-10-31 12:53:16 -0700

    Improve logic.

--- packages/plugin/src/utils/markdown.ts
+++ packages/plugin/src/utils/markdown.ts
@@ -1,3 +1,4 @@
+import type { JSONOutput } from 'typedoc';
 import { DeclarationReflectionMap } from '../types';
 
 function splitLinkText(text: string): { caption: string; target: string } {
@@ -23,24 +24,38 @@ function splitLinkText(text: string): { caption: string; target: string } {
 	};
 }
 
+function findReflectionWithMatchingTarget(
+	reflectionList: JSONOutput.DeclarationReflection[],
+	symbol: string,
+	member: string,
+) {
+	return reflectionList.find((ref) => {
+		if (ref.name !== symbol) {
+			return false;
+		}
+
+		return member === '' ? true : ref.children?.some((child) => child.name === member);
+	});
+}
+
 // TypeDoc JSON output does not replace links, so we need to do this manually.
 // @see https://github.com/TypeStrong/typedoc/blob/master/src/lib/output/plugins/MarkedLinksPlugin.ts
 export function replaceLinkTokens(markdown: string, reflections: DeclarationReflectionMap) {
 	const reflectionList = Object.values(reflections);
 
 	return markdown.replace(
-		// eslint-disable-next-line unicorn/no-unsafe-regex
-		/(?:\[(.+?)])?{@(link|linkcode|linkplain)\s+([\n.]+?)}/gi,
-		(match: string, leading: string, tagName: string, content: string): string => {
+		/{@(link|linkcode|linkplain)\s+([^}]+?)}/gi,
+		(match: string, tagName: string, content: string): string => {
 			const { caption, target } = splitLinkText(content);
 			const [symbol, member = ''] = target.split('.');
-			const reflection = reflectionList.find((ref) => ref.name === symbol);
+			const reflection = findReflectionWithMatchingTarget(reflectionList, symbol, member);
+			const label = tagName === 'linkcode' ? `\`${caption}\`` : caption;
 
 			if (!reflection || !reflection.permalink) {
-				return `\`${target}\``;
+				return label;
 			}
 
-			return `[${leading || caption}](${reflection.permalink}${member ? `#${member}` : ''})`;
+			return `[${label}](${reflection.permalink}${member ? `#${member}` : ''})`;
 		},
 	);
 }
