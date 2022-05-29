ae39220558934ab2bb7b3ed62ffbddf684a3410c
Miles Johnson <mileswjohnson@gmail.com>
2022-05-28 17:03:38 -0700

    deps(docusaurus): Update to beta.21.

--- packages/plugin/src/components/ApiChangelog.tsx
+++ packages/plugin/src/components/ApiChangelog.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { PageMetadata } from '@docusaurus/theme-common';
 import type { Props as DocItemProps } from '@theme/DocItem';
+import { TOCItem } from '../types';
 import ApiItemLayout from './ApiItemLayout';
 
 export interface ApiChangelogProps extends Pick<DocItemProps, 'route' | 'versionMetadata'> {
 	changelog: DocItemProps['content'];
 }
+
+const emptyToc: TOCItem[] = [];
 
 export default function ApiChangelog({
 	changelog: Changelog,
@@ -17,7 +20,7 @@ export default function ApiChangelog({
 			heading="Changelog"
 			pageMetadata={<PageMetadata description={Changelog.contentTitle} title="Changelog | API" />}
 			route={route}
-			toc={Changelog.toc}
+			toc={Changelog.toc ?? emptyToc}
 			versionMetadata={versionMetadata}
 		>
 			<section className="tsd-readme">
